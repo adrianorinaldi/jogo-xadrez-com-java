b97171eec989e2ecf153924af153370d2c45e039
adrianorinaldi <adriano-rinaldi@hotmail.com>
2024-06-05 19:29:18 -0300

    Iniciado implementacao classes Tabuleiro e Peca

---
+++ src/tabuleiro/Peca.java
@@ -0,0 +1,15 @@
+package tabuleiro;
+
+public class Peca {
+	
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public Peca(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+	}
+
+	protected Posicao getPosicao() {
+		return posicao;
+	}
+}
