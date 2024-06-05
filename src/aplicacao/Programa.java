3a4c9c84aed13ed561c5565400e2d2307c8be4c7
adrianorinaldi <adriano-rinaldi@hotmail.com>
2024-06-05 19:13:03 -0300

    Primeira classe Posicao

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,12 @@
+package aplicacao;
+
+import tabuleiro.Posicao;
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		Posicao position = new Posicao(3, 5);
+		System.out.println(position);
+	}
+
+}
